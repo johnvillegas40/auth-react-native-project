010eaaa326081c9167efa1fffdc1083dff8bbb9b
John Villegas <jvillegas@foxwyn.com>
2019-01-23 16:47:46 -0500

    Refactored, added switch statement to handle what is shown on the main app screen.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import {Header} from './components/common';
+import {Header, Button, Card, CardSection, Spinner} from './components/common';
 import firebase from '@firebase/app';
 import '@firebase/auth'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
+    state = { loggedIn: null}
     componentWillMount() {
         firebase.initializeApp({
             apiKey: "AIzaSyCyHP-oek2dwCMEkiSMSTj-U7hCsiErCJQ",
@@ -15,14 +16,40 @@ class App extends Component {
             storageBucket: "auth-react-jv.appspot.com",
             messagingSenderId: "169345772950"
           });
+          firebase.auth().onAuthStateChanged((user) => {
+              if(user) {
+                  this.setState({ loggedIn: true});
+              } else {
+                  this.setState({ loggedIn: false});
+              }
+          })
     }
+
+    renderContent() {
+        switch (this.state.loggedIn){
+            case true: 
+                return <Card><CardSection><Button>Test</Button></CardSection></Card>
+            case false: 
+                return <LoginForm />
+            default:
+                return <View style={styles.spinnerStyle}><Spinner size="large" /></View>
+        }        
+    }
+
+
     render() {
         return(
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                        {this.renderContent()}
             </View>
         )
+    }
+}
+
+const styles = {
+    spinnerStyle: {
+        padding: 100,
     }
 }
 
