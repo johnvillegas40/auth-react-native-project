ed560be831107aecf311177ead9e0838a96e8f29
John Villegas <jvillegas@foxwyn.com>
2019-01-23 15:01:10 -0500

    Added secureTextEntry for password entry

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '' }
+    state = { email: '', password: '' }
 
     render() {
         return(
@@ -10,11 +10,18 @@ class LoginForm extends Component {
                 <CardSection>
                     <Input
                     placeholder="user@gmail.com"
-                    label="Email"
+                    label="Email:"
                     value={this.state.email}
                     onChangeText={email => this.setState({ email })} />
                 </CardSection>
-                <CardSection />
+                <CardSection>
+                    <Input
+                    secureTextEntry={true}
+                    placeholder="password"
+                    label="Password:"
+                    value={this.state.password}
+                    onChangeText={password => this.setState({ password })} />
+                </CardSection>
 
                 <CardSection>
                     <Button>
