3525d22f9b990d05f7e5b75f1695b21b4ad1d944
John Villegas <jvillegas@foxwyn.com>
2019-01-23 13:57:58 -0500

    Added Styling, Onchange function and value props to the TextInput in the LoginForm.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import {Header} from './components/common';
-import firebase from 'firebase';
+import firebase from '@firebase/app';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
     componentWillMount() {
@@ -18,7 +19,7 @@ class App extends Component {
         return(
             <View>
                 <Header headerText="Authentication" />
-                <Text>An App!</Text>
+                <LoginForm />
             </View>
         )
     }
