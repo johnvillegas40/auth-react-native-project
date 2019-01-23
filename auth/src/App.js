c691c2579d05bc9825c8cd6b1a73c96236521822
Johnny Villegas <johnvillegas40@gmail.com>
2019-01-23 00:19:53 -0500

    Installed, set up Firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Header} from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyCyHP-oek2dwCMEkiSMSTj-U7hCsiErCJQ",
+            authDomain: "auth-react-jv.firebaseapp.com",
+            databaseURL: "https://auth-react-jv.firebaseio.com",
+            projectId: "auth-react-jv",
+            storageBucket: "auth-react-jv.appspot.com",
+            messagingSenderId: "169345772950"
+          });
+    }
     render() {
         return(
             <View>
